93f0a02ac5aea27066c15079be83912056d795ac
QuanTrLe <atl226@lehigh.edu>
2025-10-30 01:39:38 -0400

    Starting with the first part is just applying the force and moving the vertexes on itself. However havent done when to stop the vertex from moving too much and havent made it so that the force decreases over time.

--- Assets/Morphed_Sphere/MeshDeformer.cs
+++ Assets/Morphed_Sphere/MeshDeformer.cs
@@ -22,10 +22,42 @@ public class MeshDeformer : MonoBehaviour
 
         vertexVelocities = new Vector3[originalVertices.Length]; // just populating
     }
-    
-    
+
+    void Update()
+    {
+        for (int i = 0; i < displacedVertices.Length; i++)
+        {
+            UpdateVertex(i);
+        }
+        deformingMesh.vertices = displacedVertices;
+        deformingMesh.RecalculateNormals(); // new normals since vertexes got moved around
+    }
+
+    // basically just get all the forces we need and displace the vertex based on that
+    void UpdateVertex (int i) {
+		Vector3 velocity = vertexVelocities[i];
+		displacedVertices[i] += velocity * Time.deltaTime;
+	}
+
     // the thing for the input handler to call
     public void AddDeformingForce (Vector3 point, float force) {
-		Debug.DrawLine(Camera.main.transform.position, point);
+		for (int i = 0; i < displacedVertices.Length; i++) { // check every vertex
+			AddForceToVertex(i, point, force);  // then apply appropriate force
+		}
 	}
+
+    // to calc the amount of force for one vertex knowing the force and dir of it
+    void AddForceToVertex (int i, Vector3 point, float force)
+    {
+        Vector3 pointToVertex = displacedVertices[i] - point; // the relative distance from this vertex to point of contact
+
+        // calc of the force
+        // technically from inverse-square law it's just force / distance, but have 1 + distance to avoid inf the closer to the point
+        float attenuatedForce = force / (1f + pointToVertex.sqrMagnitude);
+
+        // a = F/m and delta_v = a * delta_t
+        float velocity = attenuatedForce * Time.deltaTime; // since ignoring mass (1) just do delta_v = F * delta_t 
+
+        vertexVelocities[i] += pointToVertex.normalized * velocity; // store the end result w/ dir of force in mind
+    }
 }
--- Assets/Morphed_Sphere/MeshDeformerInput.cs
+++ Assets/Morphed_Sphere/MeshDeformerInput.cs
@@ -4,6 +4,10 @@ public class MeshDeformerInput : MonoBehaviour {
 
     public float force = 10f;
 
+    // when we apply the force have to pull the origin of the force a bit away to push the vertices in
+    // basically give the deformer script a point a bit away from its actual surface & the point of contact
+    public float forceOffset = 0.1f; 
+
     void Update()
     {
         if (Input.GetMouseButton(0))
@@ -21,7 +25,8 @@ public class MeshDeformerInput : MonoBehaviour {
             MeshDeformer deformer = hit.collider.GetComponent<MeshDeformer>(); // if that smthing has a deformer script
 
             if (deformer) { // then give that deformer script the data it needs
-				Vector3 point = hit.point;
+                Vector3 point = hit.point;
+                point += hit.normal * forceOffset;
 				deformer.AddDeformingForce(point, force);
 			}
 		}
